bfe25ab7369e48456acdde47c11373f074ca7819
Andrew Ealovega <Andrew@Ealovega.dev>
2023-02-06 21:08:25 -0500

    Setup unit tests?

--- tests/unit.cpp
+++ tests/unit.cpp
@@ -1,3 +1,19 @@
-#include <rclcpp/rclcpp.hpp>
-
 #include <gtest/gtest.h>
+
+#include <rclcpp/node_options.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/utilities.hpp>
+
+#include "data_logger/data_logger_node.hpp"
+
+TEST(Some, test) {
+    rclcpp::init(0, nullptr);
+    rclcpp::NodeOptions opts;
+    dl::DataLoggerNode node{opts};
+    rclcpp::shutdown();
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
